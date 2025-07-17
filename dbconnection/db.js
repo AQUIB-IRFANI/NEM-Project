7a6f55f9e5e9037572eef7bd39da334493637587
AQUIB IRFANI <maquib1710@gmail.com>
2025-07-17 15:09:05 +0530

    db.js

--- dbconnection/db.js
+++ dbconnection/db.js
@@ -2,7 +2,10 @@ const { mongoose } = require("mongoose");
 
 const conMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/nem");
+    await mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log('connected');
   } catch (error) {
     console.log(error);
